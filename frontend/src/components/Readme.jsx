d7bee568690fd265f071ae5ce677f7069f52ac2a
Trchien123 <huynhtrungchienltt@gmail.com>
2025-06-22 20:31:29 +0700

    update demo

--- frontend/src/components/Readme.jsx
+++ frontend/src/components/Readme.jsx
@@ -86,7 +86,7 @@ const ReadMe = () => {
 
             {/* Live Demo */}
             <a
-              href=""
+              href="https://www.youtube.com/watch?v=5zaBi8Rdg5s"
               target="_blank"
               rel="noopener noreferrer"
               className="flex items-center gap-2 bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition-colors"
